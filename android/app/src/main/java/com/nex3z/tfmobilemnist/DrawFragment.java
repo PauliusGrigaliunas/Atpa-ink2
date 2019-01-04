0da7f927957f381686cd3b269fa226b8dbb181fa
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2019-01-05 00:47:24 +0200

    +

--- android/app/src/main/java/com/nex3z/tfmobilemnist/DrawFragment.java
+++ android/app/src/main/java/com/nex3z/tfmobilemnist/DrawFragment.java
@@ -4,15 +4,32 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.nex3z.fingerpaintview.FingerPaintView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DrawFragment extends Fragment {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.fpv_paint) FingerPaintView mFpvPaint;
+    @BindView(R.id.tv_prediction) TextView mTvPrediction;
+    @BindView(R.id.tv_probability) TextView mTvProbability;
+    @BindView(R.id.tv_timecost) TextView mTvTimeCost;
 
+    private Classifier mClassifier;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        ButterKnife.bind(getActivity());
+        init();
     }
 
     @Override
@@ -21,4 +38,13 @@ public class DrawFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_draw, container, false);
     }
 
+
+    private void init() {
+        try {
+            mClassifier = new Classifier(getActivity());
+        } catch (RuntimeException e) {
+            Log.e(LOG_TAG, "Failed to create classifier.", e);
+            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
 }
