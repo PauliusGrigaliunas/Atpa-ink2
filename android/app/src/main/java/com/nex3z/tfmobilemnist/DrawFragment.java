29875e7645d13a32b6f7e0c11fb45e2fef406fb2
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2019-01-05 14:16:31 +0200

    save

--- android/app/src/main/java/com/nex3z/tfmobilemnist/DrawFragment.java
+++ android/app/src/main/java/com/nex3z/tfmobilemnist/DrawFragment.java
@@ -1,13 +1,17 @@
 package com.nex3z.tfmobilemnist;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,13 +19,15 @@ import com.nex3z.fingerpaintview.FingerPaintView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DrawFragment extends Fragment {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    @BindView(R.id.fpv_paint) FingerPaintView mFpvPaint;
-    @BindView(R.id.tv_prediction) TextView mTvPrediction;
-    @BindView(R.id.tv_probability) TextView mTvProbability;
-    @BindView(R.id.tv_timecost) TextView mTvTimeCost;
+    private FingerPaintView mFpvPaint;
+    private TextView mTvPrediction;
+    private TextView mTvProbability;
+    private TextView mTvTimeCost;
+    private Button clearButton, detectButton;
 
     private Classifier mClassifier;
     @Override
@@ -35,9 +41,51 @@ public class DrawFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_draw, container, false);
-    }
 
+        final View view = inflater.inflate(R.layout.fragment_draw, container, false);
+        clearButton = (Button) view.findViewById(R.id.btn_clear2);
+        detectButton = (Button) view.findViewById(R.id.btn_detect);
+        mFpvPaint = view.findViewById(R.id.fpv_paint2);
+        mTvPrediction = view.findViewById(R.id.tv_prediction);
+        mTvProbability = view.findViewById(R.id.tv_probability);
+        mTvTimeCost = view.findViewById(R.id.tv_timecost);
+
+        clearButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mFpvPaint.clear();
+                mTvPrediction.setText(R.string.empty);
+                mTvProbability.setText(R.string.empty);
+                mTvTimeCost.setText(R.string.empty);
+            }
+        });
+
+        detectButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (mClassifier == null) {
+                    Log.e(LOG_TAG, "onDetectClick(): Classifier is not initialized");
+                    return;
+                } else if (mFpvPaint.isEmpty()) {
+                    Toast.makeText(getActivity(), R.string.please_write_a_digit, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            Bitmap image = mFpvPaint.exportToBitmap(
+                    Classifier.DIM_IMG_SIZE_WIDTH, Classifier.DIM_IMG_SIZE_HEIGHT);
+            Bitmap inverted = ImageUtil.invert(image);
+            Result result = mClassifier.classify(inverted);
+            renderResult(result);
+            }
+        });
+
+
+        return view;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mClassifier.close();
+    }
 
     private void init() {
         try {
@@ -47,4 +95,11 @@ public class DrawFragment extends Fragment {
             Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
+    private void renderResult(Result result) {
+        mTvPrediction.setText(String.valueOf(result.getNumber()));
+        mTvProbability.setText(String.valueOf(result.getProbability()));
+        mTvTimeCost.setText(String.format(getString(R.string.timecost_value),
+                result.getTimeCost()));
+    }
+
 }
--- android/app/src/main/java/com/nex3z/tfmobilemnist/Main2Activity.java
+++ android/app/src/main/java/com/nex3z/tfmobilemnist/Main2Activity.java
@@ -25,19 +25,11 @@ public class Main2Activity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Fragment fragment = new DrawFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.frame_container, fragment).commit();
+                .replace(R.id.frame_change, fragment).commit();
 
         /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
